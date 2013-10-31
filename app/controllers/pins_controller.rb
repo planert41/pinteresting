d6d772b85c74486fdfc48066f7b0fbcde017c427
Wei Zou <planert41@gmail.com>
2013-10-31 18:42:14 -0500

    Added pictures

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
 
   def index
@@ -42,16 +43,18 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
 
     def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to root_path, notice: "Not authorized to edit this pin" if @pin.nil?
+        
+        @pin = current_user.pins.find_by(id: params[:id])
+        redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
